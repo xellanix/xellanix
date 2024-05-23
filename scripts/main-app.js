5f07eff9b65c6bef9fed31766e4e53e786ca1e59
xellanix <107340353+xellanix@users.noreply.github.com>
2024-05-23 15:31:17 +0700

    Added delay function

--- scripts/main-app.js
+++ scripts/main-app.js
@@ -1,23 +1,27 @@
 function isAvailable(value = "") {
-    if (typeof value === "string" && value.length === 0) return false;
-    else if (value === null) return false;
-    else return true;
+	if (typeof value === "string" && value.length === 0) return false;
+	else if (value === null) return false;
+	else return true;
 }
 
 function substringTo(str = "", subTo = "") {
-    if (!isAvailable(str)) return null;
+	if (!isAvailable(str)) return null;
 
-    const lastIndex = str.indexOf(subTo);
-    if (lastIndex !== -1) {
-        return str.substring(0, lastIndex);
-    }
-    else {
-        return str;
-    }
+	const lastIndex = str.indexOf(subTo);
+	if (lastIndex !== -1) {
+		return str.substring(0, lastIndex);
+	} else {
+		return str;
+	}
 }
 
 function toCapitalizeEachWord(str = "", separator = " ", joinDelimiter = " ") {
-    return str.split(separator).map((word) => {
-        return word[0].toUpperCase() + word.substring(1).toLowerCase();
-    }).join(joinDelimiter);
+	return str
+		.split(separator)
+		.map((word) => {
+			return word[0].toUpperCase() + word.substring(1).toLowerCase();
+		})
+		.join(joinDelimiter);
 }
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
